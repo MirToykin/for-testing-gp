83af5f7bb7a5a9dd287ed8a8a7607853bf2e8014
MirToykin <55464650+MirToykin@users.noreply.github.com>
2019-11-13 16:27:00 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,13 +4,16 @@
 
 let trendLength = 20;
 let trendGiphyAPI = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${trendLength}`;
+// let trendGiphyAPI = `https://api.giphy.com/v1/gifs/MZoBUfHfHcMz6D6t3n?api_key=${apiKey}`;
+
 
 let trendSlidesContainer = document.querySelector('.trending__slides');
 let forwardBtn = document.querySelector('.trending__btn--forward'),
     backBtn = document.querySelector('.trending__btn--back');
 
-let trendSliderWidth = document.querySelector('.trending__slider-window').offsetWidth;
-let visibleSliderWidth = trendSliderWidth;
+let trendSliderWidth = document.querySelector('.trending__slider-window').offsetWidth;;
+let visibleSliderWidth = trendSliderWidth;;
+
 let trendSlidesWidths = [];  // в каждом элементе массива 
 //будет храниться ширина картинки + ее margin
 let allSlidesWidth;
@@ -32,13 +35,17 @@ function showTrendingGifs() {
   .then(response => response.json())
   .then(json => {
     for (let i = 0; i < trendLength; i++) {
+      
       let img = document.createElement('img');
-      img.src = json.data[i].images.original.url;
+      img.src = json.data[i].images.fixed_height.url;
       img.setAttribute('data-title', json.data[i].title);
+      img.setAttribute('data-id', json.data[i].id);
       img.className = 'trending__slide';
+      
       trendSlidesContainer.append(img);
       let coef = json.data[i].images.original.width/json.data[i].images.original.height;
       trendSlidesWidths.push(coef);
+      
     }
     
     trendSlidesWidths = trendSlidesWidths.map((item => item * slideHeight + slideMarginRight));
@@ -176,7 +183,13 @@ let searchForm = document.querySelector('.search__form'),
     searchField = document.querySelector('.search__query-field'),
     searchResultHeader = document.querySelector('.search-results__header'),
     serchResultsContainer = document.querySelector('.search-results__container'),
-    showMoreBtn = document.querySelector('.search-results__show-more');
+    showMoreBtn = document.querySelector('.search-results__show-more'),
+    searchResultCols = document.querySelectorAll('.search-results__col'),
+    searchResultCol1 = document.querySelector('.search-results__col--1'),
+    searchResultCol2 = document.querySelector('.search-results__col--2'),
+    searchResultCol3 = document.querySelector('.search-results__col--3'),
+    searchResultCol4 = document.querySelector('.search-results__col--4'),
+    searchResultCol5 = document.querySelector('.search-results__col--5');
 
 function showSearchResults(event) {
   event.preventDefault();
@@ -195,20 +208,25 @@ function showSearchResults(event) {
 }
 
 function requestGifs(queryString, stringOffset, target) {
-  let i = 0;
+  let i = 0,
+      currentCol = 0;
+
   fetch(searchGiphyAPI + queryString + stringOffset)
   .then(response => response.json())
   .then(json => {
     for (i; i < searchLimit; i++) {
 
       let img = document.createElement('img');
-      img.src = json.data[i].images.original.url;
+      img.src = json.data[i].images.fixed_height.url;
       
       if (i == 0 && stringOffset == '') {
-        while (serchResultsContainer.firstChild) {
-          serchResultsContainer.removeChild(serchResultsContainer.firstChild);
+
+        for (let i = 0; i < searchResultCols.length; i++) {
+          while (searchResultCols[i].firstChild) {
+            searchResultCols[i].removeChild(searchResultCols[i].firstChild);
+          }
         }
-        
+
         searchResultHeader.style.display = 'none';
         showMoreBtn.style.display = 'none';
         
@@ -216,7 +234,12 @@ function requestGifs(queryString, stringOffset, target) {
 
       img.className = 'search-results__item';
       img.setAttribute('data-title', json.data[i].title);
-      serchResultsContainer.append(img);
+      img.setAttribute('data-id', json.data[i].id);
+      // serchResultsContainer.append(img);
+      if (currentCol == searchResultCols.length) currentCol = 0;
+      searchResultCols[currentCol].append(img);
+
+      currentCol++;
 
     }
 
@@ -225,7 +248,8 @@ function requestGifs(queryString, stringOffset, target) {
     }
     
   })
-  .catch( () => {
+  .catch( (error) => {
+    console.log(error.message);
     if (i == 0 && target == searchForm) {
       alert("Ничего не найдено!");
       searchField.value = '';
@@ -255,29 +279,80 @@ function showMoreGifs(event) {
 
   let stringOffset = `&offset=${searchOffset}`;
 
-  requestGifs(`&q=${searchResultHeader.textContent.slice(23)}`, stringOffset, event.target)
+  requestGifs(`&q=${searchResultHeader.textContent.slice(23)}`, stringOffset, event.target) // 'Результаты по запросу: ' - 23 символа (крайний индекс 22), с 23 идет текст запроса
+}
+
+function getSearchResultsItemMarginBottom() {
+
+  let searchResultsItems = document.querySelectorAll('.search-results__item'),
+      widthOfCol = serchResultsContainer.offsetWidth * 0.192; // 19.2% ширина колонки
+
+  if (searchResultsItems.length != 0) {
+
+    if (serchResultsContainer.offsetWidth < 1000) {
+      let mb = (serchResultsContainer.offsetWidth - widthOfCol * 5)/4 + 'px'; // 5 - кол-во колонок, 4 - кол-во промежутков между колонками.
+
+      for (let i = 0; i < searchResultsItems.length; i++) {
+        searchResultsItems[i].style.marginBottom = mb;
+      }      
+    } else {
+
+      if (getComputedStyle(searchResultsItems[0]).marginBottom != '0.625em') {
+
+        for (let i = 0; i < searchResultsItems.length; i++) {
+          searchResultsItems[i].style.marginBottom = '0.625em';
+        }
+
+      }
+
+    }
+
+  }
+
 }
 
 searchForm.addEventListener('submit', showSearchResults);
 showMoreBtn.addEventListener('click', showMoreGifs);
+window.addEventListener('resize', getSearchResultsItemMarginBottom);
 
 // ___________________________Прикрепление формы поиска к верху окна__________________________
 
+let searchDiv = document.querySelector('.search');
 let topOfsearchForm = searchForm.getBoundingClientRect().top;
+let isHandled = false;
+
+function setTopWindowWidth() {
+  let topWindow = document.querySelector('.topWindow');
+  if (!topWindow) return;
+  topWindow.style.width = getComputedStyle(document.querySelector('.trending__header')).width;
+}
+
+function setSearchDivWidth() {
+  let documentWidth = document.documentElement.clientWidth;
+  
+  if (documentWidth <= 480) {
+    searchDiv.style.width = '100%';
+  }
+}
+
+window.addEventListener('resize', setTopWindowWidth);
+window.addEventListener('resize', setSearchDivWidth);
 window.addEventListener('scroll', () => {
   let windowScroll = pageYOffset;
+
   if (windowScroll > topOfsearchForm) {
     
-    searchForm.classList.add('topWindow');
-    searchField.classList.add('bgWhite');
+    searchDiv.classList.add('topWindow');
+    setTopWindowWidth();
     
   } else {
     
-    searchForm.classList.remove('topWindow');
-    searchField.classList.remove('bgWhite');
-    
+    searchDiv.classList.remove('topWindow');
+
   }
-})
+});
+
+
 
 // ___________________________увеличение изображения при клике__________________________
 
@@ -294,30 +369,45 @@ function enlargeImage(event) {
   if (event.target.className == 'trending__slide' || event.target.className == 'search-results__item') {
   
     fullScreen.style.display = 'block';
+    let gifId = event.target.getAttribute('data-id');
+    let idGiphyApi = `https://api.giphy.com/v1/gifs/${gifId}?api_key=${apiKey}`;
     
-    fullScreenImg = event.target.cloneNode();
-    fullScreenImg.className = 'full-screen__img';
-    
-    fullScreenTitle.textContent = fullScreenImg.getAttribute('data-title') == '' ? 'БЕЗ НАЗВАНИЯ' : fullScreenImg.getAttribute('data-title').toUpperCase();
-    let coef = fullScreenImg.width / fullScreenImg.height;
-    
-    fullScreenImg.height = fullScreenImg.height > 400 ? 400 : fullScreenImg.height;
-    fullScreenImg.width = fullScreenImg.height * coef; 
-    
-    fullScreenPopup.style.height = fullScreenImg.height + 50 + 'px';
-    fullScreenPopup.style.width = fullScreenImg.width + 'px';
-    
-    fullScreenImgWrap.append(fullScreenImg);
-    document.body.style.overflow = 'hidden';
-    
-    fullScreenCloseBtn.addEventListener('click', (event) => {
+    fetch(idGiphyApi)
+    .then(response => response.json())
+    .then(json => {
+      let fullScreenImgHeight = +json.data.images.original.height;
+      let fullScreenImgWidth = +json.data.images.original.width;
       
-      fullScreenImg.remove();
-      fullScreen.style.display = 'none';
-      document.body.style.overflow = '';
+      fullScreenImg = document.createElement('img')
+      fullScreenImg.src = json.data.images.original.url;
+      fullScreenImg.className = 'full-screen__img';
+      fullScreenTitle.textContent = event.target.getAttribute('data-title') == '' ? 'БЕЗ НАЗВАНИЯ' : event.target.getAttribute('data-title').toUpperCase();
       
-    });
-  
+      let coef = fullScreenImgWidth / fullScreenImgHeight;
+    
+      fullScreenImg.height = fullScreenImgHeight > 400 ? 400 : fullScreenImgHeight;
+      fullScreenImg.width = fullScreenImg.height * coef; 
+
+      fullScreenPopup.style.height = fullScreenImg.height + 50 + 'px';
+      fullScreenPopup.style.width = fullScreenImg.width + 'px';
+
+      fullScreenImgWrap.append(fullScreenImg);
+      document.body.style.overflow = 'hidden';
+
+      fullScreenCloseBtn.addEventListener('click', (event) => {
+
+        fullScreenImg.remove();
+        fullScreen.style.display = 'none';
+        document.body.style.overflow = '';
+        fullScreenPopup.style.height = 0;
+
+      });
+      
+    })
+    .catch( () => {
+      alert('Изображение не найдено!')
+    })
+
   }
 }
 
